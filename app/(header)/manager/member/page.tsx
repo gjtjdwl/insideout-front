c6d5825f140fd72e69ea0be4166ed46f41ddae47
kimseongmi <kessm6529@naver.com>
2025-01-21 16:21:46 +0900

    feat : 매니저 페이지 구성

---
+++ app/(header)/manager/member/page.tsx
@@ -0,0 +1,15 @@
+export default function member() {
+  return (
+    <div className="bg-customPink px-4 sm:px-[50px]">
+      <div className="items-center bg-white w-full p-10 min-h-[50vh]">
+        <div className="flex items-center justify-between p-4 border-b">
+          <div
+            className="font-bold text-xl md:text-3xl"
+          >
+            부서원 관리  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
