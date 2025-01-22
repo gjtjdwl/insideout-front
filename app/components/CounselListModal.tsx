bec5db87ac785c7f193e8c30f3f5fc9f04cebcce
gjtjdwl <panggoon6@gmail.com>
2025-01-22 13:48:59 +0900

    feat: 상담 기록 모달에 css 적용

--- app/components/CounselListModal.tsx
+++ app/components/CounselListModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import mock from '@/app/counselmock.json';
-
+import { StarIcon } from '@heroicons/react/24/outline';
 interface CounselModalProps {
   onClose: () => void;
 }
@@ -17,6 +17,9 @@ function dateFormat(date: string): string {
   return formatDate;
 }
 const CounselListModal: React.FC<CounselModalProps> = ({ onClose }) => {
+  const handleDelete = () => {
+    alert('상담기록이 삭제 되었습니다');
+  };
   return (
     <div className="z-50 fixed inset-0 flex items-center justify-center bg-black bg-opacity-30">
       <motion.div
@@ -24,18 +27,22 @@ const CounselListModal: React.FC<CounselModalProps> = ({ onClose }) => {
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.9 }}
         transition={{ duration: 0.3 }}
-        className="bg-white rounded-lg shadow-lg p-6 max-w-sm"
+        className="bg-white rounded-lg shadow-lg p-6 max-w-md"
       >
         <p className="text-center text-lg font-semibold">상담 기록</p>
 
-        <ul role="list" className="mt-10 min-h-[40vh] mx-10">
+        <ul role="list" className="mt-10 min-h-[40vh] mx-6">
           {mock.map((item) => {
             let date = dateFormat(item.createdAt);
             return (
-              <li key={item.sessionId} className="gap-x-10">
-                <div className="flex gap-x-4">
-                  <p className="font-medium p-1 text-gray-900 mr-6">{date}</p>
-                  <button className="border rounded-lg bg-red-500 px-2 py-1">
+              <li key={item.sessionId} className="my-2">
+                <div className="flex justify-center items-center px-4 py-3 gap-x-4 border border-black rounded-md">
+                  <StarIcon className=" h-6 w-6" />
+                  <div className="font-medium  text-gray-900 mr-6">{date}</div>
+                  <button
+                    onClick={handleDelete}
+                    className="border rounded-lg px-3 py-[1px] text-white bg-red-500 "
+                  >
                     삭제
                   </button>
                 </div>
