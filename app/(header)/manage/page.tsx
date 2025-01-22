698a9efd42222cb8500545dd75a0af67f04230cc
kimseongmi <kessm6529@naver.com>
2025-01-22 14:19:24 +0900

    design : 반응형

--- app/(header)/manage/page.tsx
+++ app/(header)/manage/page.tsx
@@ -8,36 +8,45 @@ export default function managerAdminPage() {
   return (
     <>
       <div className="bg-customPink px-4 sm:px-[50px]">
-        <div className="bg-white px-44 py-16">
+        <div className="items-center bg-white w-full p-10 min-h-[50vh]">
           <div className="flex items-center justify-between p-4 border-b">
             <div className="font-bold text-xl md:text-3xl">부서 관리</div>
           </div>
-          <div className="mt-16 font-medium text-2xl">부서 통계</div>
-          <div className="flex justify-center">
-            <div>
-              <Image
-                src="/graph.png"
-                alt="그래프 들어갈 자리"
-                width={500}
-                height={300}
-                style={{ height: '330px' }}
-                className="ml-20"
-              />
-              <div className="text-2xl text-center pl-20 pt-10">
-                전 월 대비 ORS 점수 변동량 : - 400%
+          <div className="m-10">
+            <div className="mt-16 font-medium text-lg md:text-2xl">
+              부서 통계
+            </div>
+            <div className="grid grid-flow-col gap-x-8 justify-center">
+              <div className="flex flex-col items-end">
+                <Image
+                  src="/graph.png"
+                  alt="그래프 들어갈 자리"
+                  width={500}
+                  height={300}
+                  style={{ height: '330px' }}
+                  className=""
+                />
+                <div className="mt-5 text-base lg:text-2xl text-center">
+                  전 월 대비 ORS 점수 변동량 : - 400%
+                </div>
+              </div>
+              <div className="flex flex-col text-center items-cneter ">
+                <div className="p-10 border border-[#525252] h-[80%] flex flex-col items-start justify-center">
+                  <div className="text-base lg:text-2xl ">
+                    ORS 점수 평균 : 36점
+                  </div>
+                  <div className="text-base lg:text-2xl">ORS 점수 분산 : 4</div>
+                </div>
               </div>
             </div>
-            <div className="flex flex-col text-center  items-cneter w-[50%] pl-24">
-              <div className="border border-black">
-                <div className="text-2xl mt-28">ORS 점수 평균 : 36점</div>
-                <div className="text-2xl mt-4 mb-32">ORS 점수 분산 : 4</div>
-              </div>
+            <div className="mt-24 font-medium text-lg lg:text-2xl">부서원</div>
+            <div className="grid grid-cols-2 gap-x-6 gap-y-5 mt-9">
+              <DepartmentCard route={route} name={'부서원1'} id={'아이디1'} />
+              <DepartmentCard route={route} name={'부서원2'} id={'아이디2'} />
+              <DepartmentCard route={route} name={'부서원2'} id={'아이디2'} />
+              <DepartmentCard route={route} name={'부서원2'} id={'아이디2'} />
             </div>
           </div>
-          <div className="mt-24 font-medium text-2xl">부서원</div>
-
-          <DepartmentCard route={route} name={'부서원1'} id={'아이디1'} />
-          <DepartmentCard route={route} name={'부서원2'} id={'아이디2'} />
         </div>
       </div>
     </>
--- app/components/DepartmentCard.tsx
+++ app/components/DepartmentCard.tsx
@@ -18,16 +18,16 @@ export default function DepartmentCard({
   };
   return (
     <>
-      <div className="relative mt-8 px-8 flex justify-between border rounded-md border-gray-400">
-        <div className="py-8">
-          <div className="text-xl font-bold">{name}</div>
-          <div className="mt-2 text">{id}</div>
+      <div className="relative min-h-[100px] p-4 lg:p-8 flex justify-between items-center border rounded-md border-gray-400">
+        <div className="flex flex-col">
+          <div className="text-sm md-text-2xl  font-bold">{name}</div>
+          <div className="mt-2 text-xs md-text-xl">{id}</div>
         </div>
-        <Cog6ToothIcon className="absolute h-6 w-6 top-3 right-4" />
-        <div className="py-14">
+        <Cog6ToothIcon className="absolute h-6 md:w-5 w-4 top-3 right-4" />
+        <div className="">
           <button
             onClick={handleClick}
-            className="bg-customPink w-[200px] text-black font-semibold py-2 rounded-lg hover:bg-customPinkHover focus:outline-none"
+            className="text-xs md:text-base bg-customPink max-w-[200px] px-6 lg:px-14 text-black font-semibold py-2 lg:py-3 rounded-lg hover:bg-customPinkHover focus:outline-none"
           >
             세부사항
           </button>
